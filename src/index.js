9ce7d83dcd51be1c20fa9956fd0c93c3aa4123d5
Reese Sorrell <joreesesorrell@gmail.com>
2023-03-23 23:01:51 -0500

    Format board grid on screen

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import "./style.css";
 import displayAdder from "display-adder-reese";
+import game from "./helpers/gameloop";
+import { makeBoard } from "./helpers/domBoardBuilder";
 
-displayAdder.createDiv(document.body, 'hey there', 'red');
+const realGame = game('Reese');
+realGame.setUpTestGame();
+
+const content = displayAdder.createDiv(document.body, '', 'content');
+
+const boardHolder = displayAdder.createDiv(content, '', 'player-board-holder', 'board-holder');
+makeBoard(boardHolder, 'player');
+
+const computerBoardHolder = displayAdder.createDiv(content, '', 'computer-board-holder', 'board-holder');
+makeBoard(computerBoardHolder, 'computer');
